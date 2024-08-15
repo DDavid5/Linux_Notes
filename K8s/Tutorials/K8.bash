f3677f9128919ef5f3d9bc6e03faed3a45d8b476
DDavid5 <104031136+DDavid5@users.noreply.github.com>
2024-08-15 10:05:57 +0200

    Doplnene o kubectx a Kubens - nastroje na zmenu NS

--- K8s/Tutorials/K8.bash
+++ K8s/Tutorials/K8.bash
@@ -291,7 +291,37 @@ spec:
     limits.cpu: "10"
     limits.memory: 10Gi
 	
+----Utility Kubectx
+#With this tool, you don nott have to make use of lengthy “kubectl config” commands to switch between contexts. This tool is particularly useful to switch context between clusters in a multi-cluster environment.
 
+Installation:
+
+    sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+    sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+_________________________
+///Syntax:
+To list all contexts:
+    kubectx
+
+To switch to a new context:
+    kubectx <context_name>
+
+To switch back to previous context:
+    kubectx -
+
+To see current context:
+    kubectx -c
+
+----Utility Kubens
+#This tool allows users to switch between namespaces quickly with a simple command
+///Syntax:
+
+To switch to a new namespace:
+
+    kubens <new_namespace>
+
+To switch back to previous namespace:
+    kubens -
 
 ====================SCHEDULER/Binding====================
 #v YAML subore mozeme urcit na akom node sa ma pod vytvorit tym ze pridame nodeName pod
