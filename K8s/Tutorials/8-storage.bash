e1b6bda9ad3afad85300c168691b99e22321acfb
David Dopirak <david.dopirak5@gmail.com>
2024-08-16 12:26:05 +0200

    Pridane PV a PVC

--- K8s/Tutorials/8-storage.bash
+++ K8s/Tutorials/8-storage.bash
@@ -2,7 +2,7 @@
 #vramci nodu sa volumes ukladaju do /data/
 #volumes vytvarame len na 1 Node Cluser
 #ak chceme mat volumes pre multiNode Cluster - pouzijeme nejaky externe riesenie - GluserFS, NFS, Flocker, Fiberchanel, ceph, AWS
-#volumes zapiseme vramci objektu do
+#volumes zapiseme vramci pod definiton do
 spec
   volumes:
   - name: data-volume
@@ -19,12 +19,27 @@ spec
  =======Persistent Volumes=======
  #) je časť úložiska v klastri, ktorú poskytol správca alebo ktorá bola dynamicky poskytnutá pomocou tried úložísk
  #urcite nou kolko ma byt vyhradneho priesotru
+ #pool z ktoreho si system automaticky vyberie vhodny PV - na zaklade poziadavky podu
+ #zadava Administrator a uzivatel nasledne nakonfiguruje PVC - ktora si vyberie vhodny PV
  kind: PersistentVolume
  spec:
-   accessModes:
+	accessModes:
     - ReadWriteOnce
-
+	capacity:
+		storage: 1Gi
+	hostPath: #nepouzivame v produkci, namiesto toho pouzivame cloud solution napr
+	awsElasticBlockStore:
+		volumeID: <volume-ID>
+		fsType: ext4
+   
 =======Persistent Volumes Claims=======
  #je požadavek uživatele na úložiště. Je podobný jako Pod. Pody spotřebovávají prostředky uzlu a PVC spotřebovávají prostředky PV
  #sluzi na spojenie s PV
- persistentVolumeTrvlsimPolicy #urcime co 
+ #kazda PVC moze mat len jedno spojenie (bind) s PV
+ kind: PersistentVolume
+ spec:
+	accessModes:
+		- ReadWriteOnce
+	resoruces:
+		requests
+			storage: 500Mi
